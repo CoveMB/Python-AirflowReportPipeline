2e0f0114798936a359062ae2d38f56415fc0168d
BjMrq <44323490+BjMrq@users.noreply.github.com>
2019-04-19 19:51:05 -0400

    Update Travis

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -62,38 +62,32 @@ export \
   AIRFLOW__CELERY__RESULT_BACKEND \
 
 
-if [ "$TRAVIS" = "Travis" ]; then
-  airflow initdb
-  sleep 10
-  python -m unittest discover tests
-else
-  case "$1" in
-    webserver)
-      airflow initdb
-      sleep 10
-      python -m unittest discover tests
-      sleep 10
-      if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ]; then
-        # With the "Local" executor it should all run in one container.
-        airflow scheduler &
-      fi
-      exec airflow webserver
-      ;;
-    worker|scheduler)
-      # To give the webserver time to run initdb.
-      sleep 10
-      exec airflow "$@"
-      ;;
-    flower)
-      sleep 10
-      exec airflow "$@"
-      ;;
-    version)
-      exec airflow "$@"
-      ;;
-    *)
-      # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
-      exec "$@"
-      ;;
-  esac
-fi
+case "$1" in
+  webserver)
+    airflow initdb
+    sleep 10
+    python -m unittest discover tests
+    sleep 10
+    if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ]; then
+      # With the "Local" executor it should all run in one container.
+      airflow scheduler &
+    fi
+    exec airflow webserver
+    ;;
+  worker|scheduler)
+    # To give the webserver time to run initdb.
+    sleep 10
+    exec airflow "$@"
+    ;;
+  flower)
+    sleep 10
+    exec airflow "$@"
+    ;;
+  version)
+    exec airflow "$@"
+    ;;
+  *)
+    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
+    exec "$@"
+    ;;
+esac
